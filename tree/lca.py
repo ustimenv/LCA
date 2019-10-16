940c981090fdc0db924979e9552c3e89c75bc504
ustimenv <ustimenkovlad@gmail.com>
2019-10-16 18:36:28 +0100

    Tests basically done

--- tree/lca.py
+++ tree/lca.py
@@ -1,13 +1,13 @@
-from treeNode import TreeNode
+from treeNode import TreeNodeV1, TreeNodeV2
 
 
 class LCA:
 
     @staticmethod
-    def findLCA(tree, A, B):
+    def findLcaV1(tree, A, B):
         """
 
-        :param tree: Tree or subtree we are to search
+        :param tree: Binary Tree or subtree we are to search
         :param A:   A value of a node in the tree
         :param B:   Value of another node's in the tree
         If either A or B are not in the tree, the LCA is that node
@@ -16,8 +16,8 @@ class LCA:
         if tree is None or A == tree.data or B == tree.data:
             return tree
 
-        leftTree = LCA.findLCA(tree.left, A, B)
-        rightTree = LCA.findLCA(tree.right, A, B)
+        leftTree = LCA.findLcaV1(tree.left, A, B)
+        rightTree = LCA.findLcaV1(tree.right, A, B)
 
         if leftTree is None and rightTree is None:
             return None
@@ -29,3 +29,15 @@ class LCA:
             return rightTree
         else:
             return leftTree
+
+    @staticmethod
+    def findLcaV2(tree, A, B):
+        """
+
+        :param tree: Directed Acyclic Graph we are searching
+        :param A: A value of a node in the graph
+        :param B: Another value of a node in the graph
+        """
+        return TreeNode2(data=None)
+
+
--- tree/lcaTest.py
+++ tree/lcaTest.py
@@ -1,15 +1,15 @@
 import unittest
 
 from lca import LCA
-from treeNode import TreeNode
+from treeNode import TreeNodeV1, TreeNodeV2
 
-T = TreeNode(3)
-T.left = TreeNode(2)
-T.left.left = TreeNode(1)
-T.left.left.left = TreeNode(0.2)
-T.left.right = TreeNode(2.5)
-T.right = TreeNode(4)
-T.right.right = TreeNode(8)
+Tv1 = TreeNodeV1(3)
+Tv1.left = TreeNodeV1(2)
+Tv1.left.left = TreeNodeV1(1)
+Tv1.left.left.left = TreeNodeV1(0.2)
+Tv1.left.right = TreeNodeV1(2.5)
+Tv1.right = TreeNodeV1(4)
+Tv1.right.right = TreeNodeV1(8)
 
 '''
             3
@@ -21,22 +21,50 @@ T.right.right = TreeNode(8)
      0.2   
 '''
 
+a = TreeNodeV2('A')
+b = TreeNodeV2('B')
+c = TreeNodeV2('C')
+d = TreeNodeV2('D')
+e = TreeNodeV2('E')
+f = TreeNodeV2('F')
+g = TreeNodeV2('G')
+a.children = {b, c}
+b.children = {d}
+c.children = {d, e, f}
+d.children = {g}
+e.children = {g}
+f.children = {}
+g.children = {}
+
 
 class LCATester(unittest.TestCase):
-    def testLCA(self):
-        self.assertEqual(LCA.findLCA(T, 1, 2.5).data, 2)
-        self.assertEqual(LCA.findLCA(T, 3, 3).data, 3)
-        self.assertEqual(LCA.findLCA(T, 8, 2.5).data, 3)
-        self.assertEqual(LCA.findLCA(T, 4, 3).data, 3)
-        self.assertEqual(LCA.findLCA(T, 10, 2).data, 2)
-        self.assertEqual(LCA.findLCA(T, 2, 1).data, 2)
-        self.assertEqual(LCA.findLCA(T, 8, 0.2).data, 3)
+    def testLcaV1(self):
+        self.assertEqual(LCA.findLcaV1(Tv1, 1, 2.5).data, 2)
+        self.assertEqual(LCA.findLcaV1(Tv1, 3, 3).data, 3)
+        self.assertEqual(LCA.findLcaV1(Tv1, 8, 2.5).data, 3)
+        self.assertEqual(LCA.findLcaV1(Tv1, 4, 3).data, 3)
+        self.assertEqual(LCA.findLcaV1(Tv1, 10, 2).data, 2)
+        self.assertEqual(LCA.findLcaV1(Tv1, 2, 1).data, 2)
+        self.assertEqual(LCA.findLcaV1(Tv1, 8, 0.2).data, 3)
 
-    def testBroken(self):
-        self.assertEqual(LCA.findLCA(T, 1, None).data, 1)
-        self.assertIsNone(LCA.findLCA(None, 1, 2.5))
-        self.assertIsNone(LCA.findLCA(T, None, None))
-        self.assertIsNone(LCA.findLCA(None, None, None))
+    def testBrokenV1(self):
+        self.assertEqual(LCA.findLcaV1(Tv1, 1, None).data, 1)
+        self.assertIsNone(LCA.findLcaV1(None, 1, 2.5))
+        self.assertIsNone(LCA.findLcaV1(Tv1, None, None))
+        self.assertIsNone(LCA.findLcaV1(None, None, None))
+
+    def testLcaV2(self):
+        self.assertEqual(LCA.findLcaV2('A', 'D', 'F').data, 'C')
+        self.assertEqual(LCA.findLcaV2('A', 'G', 'A').data, 'A')
+        self.assertEqual(LCA.findLcaV2('A', 'D', 'G').data, 'D')
+        self.assertEqual(LCA.findLcaV2('A', 'B', 'F').data, 'A')
+        self.assertEqual(LCA.findLcaV2('A', 'E', 'D').data, 'C')
+
+    def testBrokenV2(self):
+        self.assertIsNone(LCA.findLcaV2('A', 1, None))
+        self.assertIsNone(LCA.findLcaV2(None, 1, 2.5))
+        self.assertIsNone(LCA.findLcaV2('A', None, None))
+        self.assertIsNone(LCA.findLcaV2(None, None, None))
 
 
 if __name__ == "__main__":
